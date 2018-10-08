25e02f1ce72237b012be951e423ac75c882bfd8a
kret0 <mjp.sik@gmail.com>
2018-10-08 11:00:07 +0200

    Commit biggest bet

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 300
+        return 1000
 
     def showdown(self, game_state):
         pass
