e4afc509a27e2c5795171098d2d9f8e1cadf2448
kret0 <mjp.sik@gmail.com>
2018-10-08 11:41:06 +0200

    PanicMode

--- player.py
+++ player.py
@@ -2,9 +2,11 @@
 
 class Player:
     VERSION = "Default Python folding player"
+    def panicMode(self): #default game mode
+        return 1000
 
     def betRequest(self, game_state):
-        return 1000
+        return self.panicMode(self)
 
     def showdown(self, game_state):
         pass
