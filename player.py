2955345cf828fb248d4359245575389fb2e5c722
Paul <bielcu@gmail.com>
2018-10-08 14:17:41 +0200

    fix returning bet logic

--- player.py
+++ player.py
@@ -26,15 +26,20 @@ class Player:
         try:
             me = self.getPlayer("Hold Em All")
             mycards = self.getCardsFromPlayer(me)
-            self.checkForPairInHand(mycards)
-            return self.getMyCoinStack()/6
+            bet = self.checkForPairInHand(mycards)
+            
+            if bet == True:
+                return self.getMyCoinStack()/2
+            else:
+                return 10
+
         except Exception as e:
             traceback.print_exc()
             return 1000
 
     def checkForPairInHand(self, mycards):
         if mycards[0]["rank"] == mycards[1]["rank"]:
-            return self.getMyCoinStack() / 2
+            return True
 
     def showdown(self, game_state):
         pass
