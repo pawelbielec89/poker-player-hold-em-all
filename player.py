d97371daac8c148495098e72411d5546fe5ea4f3
Paul <bielcu@gmail.com>
2018-10-08 14:59:19 +0200

    Add checkForPoker method

--- player.py
+++ player.py
@@ -26,14 +26,24 @@ class Player:
         if mycards[0]["rank"] == mycards[1]["rank"]:
             return True
 
-    def checkForPairInHandAndCommunity(self, mycards, communitycards):
+    def checkForPairInHandAndCommunity(self, cards, communitycards):
         if communitycards.length == 0:
             return False
-        for card in mycards:
+        for card in cards:
             for communitycard in communitycards:
                 if card["rank"] == communitycard["rank"]:
                     return True
 
+    def checkForPoker(self, cards, communitycards):
+        player_and_community_cards = []
+        for card in cards:
+            player_and_community_cards += card["rank"]
+        for card in communitycards:
+            player_and_community_cards += card["rank"]
+        if "A" in player_and_community_cards and "K" in player_and_community_cards and "Q" in player_and_community_cards and "J" in player_and_community_cards and "10" in player_and_community_cards:
+            return True
+        else: return False
+        
     def betRequest(self, game_state):
         self.game_state = game_state
         try:
@@ -50,7 +60,7 @@ class Player:
 
         except Exception as e:
             traceback.print_exc()
-            return 1000
+            return 333
                 
     def showdown(self, game_state):
         pass
